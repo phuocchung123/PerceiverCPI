f5bc4bbc4a0a87174454c99c7955fc9a3dcaa236
phuocchung123 <chungnvp197@gmail.com>
2025-12-01 09:50:20 +0700

    mofify train.py with try to turn off

--- train.py
+++ train.py
@@ -1,6 +1,14 @@
 """Trains a chemprop model on a dataset."""
 
 from chemprop.train import chemprop_train
+from rdkit import RDLogger
+import warnings
+
+# Disable RDKit logging (C++ level)
+RDLogger.DisableLog('rdApp.*')
+
+# Disable Python warnings (optional, for other library warnings)
+warnings.filterwarnings("ignore")
 
 
 if __name__ == '__main__':
